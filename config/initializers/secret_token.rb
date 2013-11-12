868857b8a7919193a55aa4dcec4fac8ec95c6163
Adam Jones <adam.jones@yourenquirymanager.com.au>
2013-11-13 09:38:47 +1000

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StudentManager::Application.config.secret_key_base = '6d2b78f7e1eb8e8ba887151e07bae4a18cd02f4c3dcdcba2f4873dcc8bc132f4c79bbc6afc0cd35fbca37c1694182cd54339d86897d7ddb01a1ac5ee701098f5'
