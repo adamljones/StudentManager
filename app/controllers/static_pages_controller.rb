17208abb52d9a4dd770abb774d742108f2a8cdba
Adam Jones <adam.jones@yourenquirymanager.com.au>
2013-12-16 11:07:40 +1000

    Add in temp static page.

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,6 @@
+class StaticPagesController < ApplicationController
+
+  def form2
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ StudentManager::Application.routes.draw do
 
   resources :instructors
 
+  get 'form2' => 'static_pages#form2'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
